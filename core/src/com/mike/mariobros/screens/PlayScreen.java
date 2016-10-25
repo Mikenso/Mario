0e2da921bc0d81cdf3bd92159b960cfebd4d400d
Mike <gromyko@cn.ru>
2016-10-25 15:34:27 +0700

    some changes

---
+++ core/src/com/mike/mariobros/screens/PlayScreen.java
@@ -0,0 +1,72 @@
+package com.mike.mariobros.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mike.mariobros.MarioBros;
+import com.mike.mariobros.scenes.Hud;
+
+
+/**
+ * Created by Mike on 25.10.2016.
+ */
+public class PlayScreen implements Screen {
+
+    private MarioBros game;
+    private Hud hud;
+    private Viewport gamePort;
+    private OrthographicCamera gameCam;
+
+
+    public PlayScreen(MarioBros game) {
+        this.game = game;
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport( MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gameCam);
+        hud = new Hud(game.batch);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0 ,1 );
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+     gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
