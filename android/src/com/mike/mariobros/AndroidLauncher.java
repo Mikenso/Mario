46b8419f0e4b9242a062b125ef5b87f1a517cd6f
Mike <gromyko@cn.ru>
2016-10-25 14:07:51 +0700

    Initial commit

---
+++ android/src/com/mike/mariobros/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mike.mariobros;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mike.mariobros.MarioBros;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MarioBros(), config);
+	}
+}
