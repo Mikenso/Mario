be9f970e026a54929f2c317776c24cd2ca5c240d
Mike <gromyko@cn.ru>
2016-10-26 16:25:23 +0700

    some changes

--- core/src/com/mike/mariobros/screens/PlayScreen.java
+++ core/src/com/mike/mariobros/screens/PlayScreen.java
@@ -5,6 +5,9 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
@@ -20,6 +23,9 @@ public class PlayScreen implements Screen {
 
     private MarioBros game;
     private Hud hud;
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
     private Viewport gamePort;
     private OrthographicCamera gameCam;
 
@@ -29,6 +35,23 @@ public class PlayScreen implements Screen {
         gameCam = new OrthographicCamera();
         gamePort = new FitViewport( MarioBros.V_WIDTH, MarioBros.V_HEIGHT, gameCam);
         hud = new Hud(game.batch);
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+        gameCam.position.set(gamePort.getWorldWidth() / 2 , gamePort.getWorldHeight() / 2 , 0 );
+
+    }
+
+    public void update (float dt) {
+        hadleInput(dt);
+        gameCam.update();
+        renderer.setView(gameCam);
+    }
+
+    private void hadleInput(float dt) {
+        if (Gdx.input.isTouched()) {
+            gameCam.position.x += 100 * dt;
+        }
     }
 
     @Override
@@ -38,8 +61,12 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        update(delta);
+
         Gdx.gl.glClearColor(0, 0, 0 ,1 );
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
         game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
         hud.stage.draw();
 
